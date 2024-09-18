b3ae8883af098678f8a79004fcd4ac0198588252
Aiste <icy.aiste@gmail.com>
2024-09-18 15:30:16 +0200

    test suite for rendering of searched word

--- Dictionary2/Dictionary/src/App.test.tsx
+++ Dictionary2/Dictionary/src/App.test.tsx
@@ -1,5 +1,5 @@
 import App from './App';
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 
 test('should render headline, input field and a search button upon loading', () => {
   render(<App />);
@@ -8,21 +8,45 @@ test('should render headline, input field and a search button upon loading', () 
   expect(screen.getByRole('button', { name: 'Search' })).toBeInTheDocument();
 });
 
-test('should render error message if user tries to search with empty input field', async () => {
-  render(<App />);
+describe('searching for words functionality', () => {
+  test('should render error message if user tries to search with empty input field', async () => {
+    render(<App />);
 
-  //Find input elem
-  const inputElement = screen.getByRole('textbox');
-  expect(inputElement).toBeInTheDocument();
+    //Find input elem
+    const inputElement = screen.getByRole('textbox');
+    expect(inputElement).toBeInTheDocument();
+
+    //find the search button and click it
+    const searchButton = screen.getByRole('button', { name: 'Search' });
+    expect(searchButton).toBeInTheDocument();
+
+    //click the search button
+    await act(async () => {
+      searchButton.click();
+    });
+    const errorMessage = screen.getByText('Please enter a word');
+    expect(errorMessage).toBeInTheDocument();
+  });
+  test.only('should render a list of words when user searches for a word', async () => {
+    render(<App />);
+     //Find input elem
+    const inputElement = screen.getByRole('textbox');
+    expect(inputElement).toBeInTheDocument();
+    //simulate typing word into input field
+    await act(async () => {
+      fireEvent.change(inputElement, { target: { value: 'kitten' } })
+  });
+  expect((inputElement as HTMLInputElement).value).toBe('kitten');
 
   //find the search button and click it
-  const searchButton = screen.getByRole('button', { name: 'Search' });
-  expect(searchButton).toBeInTheDocument();
+  const searchBtn = screen.getByRole('button', { name: 'Search' });
+  expect(searchBtn).toBeInTheDocument();
 
-  //click the search button
   await act(async () => {
-    searchButton.click();
+    searchBtn.click();
   });
-  const errorMessage = screen.getByText('Please enter a word');
-  expect(errorMessage).toBeInTheDocument();
+
+  //check that word(list) is rendered
+  await screen.findByText('kitten');
+});
 });
