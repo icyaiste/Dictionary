f5511447f2fa975a33a19d67ca90d1147f3ef1a4
Aiste <icy.aiste@gmail.com>
2024-09-27 15:56:46 +0200

    migrated to msw and configured tests to use userEvent

--- Dictionary2/Dictionary/src/components/Favorites/Favorites.test.tsx
+++ Dictionary2/Dictionary/src/components/Favorites/Favorites.test.tsx
@@ -1,37 +1,74 @@
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../../App';
+import userEvent from '@testing-library/user-event';
+import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
+
+const server = setupServer(
+  http.get('https://api.dictionaryapi.dev/api/v2/entries/en/kitten', () =>
+    HttpResponse.json([
+      {
+        word: 'kitten',
+        phonetic: '/ˈkɪtən/',
+        phonetics: [
+          {
+            text: '/ˈkɪtən/',
+            audio:
+              'https://api.dictionaryapi.dev/media/pronunciations/en/kitten-uk.mp3',
+          },
+        ],
+        meanings: [
+          {
+            partOfSpeech: 'noun',
+            definitions: [
+              {
+                definition:
+                  'A young cat, especially before sexual maturity (reached at about seven months).',
+              },
+              {
+                definition:
+                  'A young rabbit, rat, hedgehog, squirrel, fox, beaver, badger, etc.',
+              },
+              {
+                definition: 'A moth of the genus Furcula.',
+              },
+              {
+                definition: 'A term of endearment, especially for a woman.',
+              },
+            ],
+          },
+          {
+            partOfSpeech: 'verb',
+            definitions: [
+              {
+                definition: 'To give birth to kittens.',
+              },
+            ],
+          },
+        ],
+      },
+    ])
+  )
+);
+
+beforeAll(() => server.listen());
+
+afterAll(() => server.close());
+
 
 describe('adding and deleting words to/from favorites', () => {
     test('add word to favorites', async () => {
-      //Mock the fetch function
-      const mockFetch = vi.fn().mockResolvedValueOnce({
-        ok: true,
-        json: async () => [
-          {
-            word: 'kitten',
-            meanings: [],
-          },
-        ],
-      });
-      global.fetch = mockFetch; //replace fetch with mockFetch
-  
+     const user = userEvent.setup();
       render(<App />);
+
       //Find input elem
       const inputElement = screen.getByRole('textbox');
-      expect(inputElement).toBeInTheDocument();
-  
       //simulate typing word into input field
-      await act(async () => {
-        fireEvent.change(inputElement, { target: { value: 'kitten' } });
-      });
+     await user.type(inputElement,'kitten');
       expect((inputElement as HTMLInputElement).value).toBe('kitten');
+      //find the search button and click it
       const searchBtn = screen.getByRole('button', { name: 'Search' });
-      expect(searchBtn).toBeInTheDocument();
-  
-      //click search
-      await act(async () => {
-        searchBtn.click();
-      });
+      await user.click(searchBtn);
   
       //check that word(list) is rendered
       await screen.findByText('kitten');
@@ -39,10 +76,7 @@ describe('adding and deleting words to/from favorites', () => {
   
       //add word to favorites
       const addToFavoritesButton = screen.getByText('Add to favorites');
-      expect(addToFavoritesButton).toBeInTheDocument();
-      await act(async () => {
-        fireEvent.click(addToFavoritesButton);
-      });
+      await user.click(addToFavoritesButton);
   
       // Check that the word has been added to favorites
       const favoriteWords = await screen.findAllByRole('heading', { level: 3 }); // Favorite items are rendered as h3 elements
@@ -54,45 +88,23 @@ describe('adding and deleting words to/from favorites', () => {
     });
   
     test('delete word from favorites', async () => {
-      //Mock the fetch function
-      const mockFetch = vi.fn().mockResolvedValueOnce({
-        ok: true,
-        json: async () => [
-          {
-            word: 'kitten',
-            meanings: [],
-          },
-        ],
-      });
-      global.fetch = mockFetch; //replace fetch with mockFetch
+      const user = userEvent.setup();
       render(<App />);
-      //Find input elem
+
+      //Find input elem and type in word
       const inputElement = screen.getByRole('textbox');
-      expect(inputElement).toBeInTheDocument();
-  
-      //simulate typing word into input field
-      await act(async () => {
-        fireEvent.change(inputElement, { target: { value: 'kitten' } });
-      });
-      expect((inputElement as HTMLInputElement).value).toBe('kitten');
+      await user.type(inputElement,'kitten');
+
+      //find the search button and click it
       const searchBtn = screen.getByRole('button', { name: 'Search' });
-      expect(searchBtn).toBeInTheDocument();
+      await user.click(searchBtn);
   
-      //click search
-      await act(async () => {
-        searchBtn.click();
-      });
-  
-      //check that word(list) is rendered
-      await screen.findAllByText('kitten');
+      //check that buutton 'add to favorites' is rendered
       await screen.findByText('Add to favorites');
   
-      //add word to favorites
+      //add word to favorites list
       const addToFavoritesButton = screen.getByText('Add to favorites');
-      expect(addToFavoritesButton).toBeInTheDocument();
-      await act(async () => {
-        fireEvent.click(addToFavoritesButton);
-      });
+      await user.click(addToFavoritesButton);
   
       // Check that the word has been added to favorites
       const favoriteWords = await screen.findAllByRole('heading', { level: 3 }); // Favorite items are rendered as h3 elements
@@ -101,15 +113,11 @@ describe('adding and deleting words to/from favorites', () => {
       // Check that the word 'kitten' is in the favorites list
       const addedFavoriteWord = screen.getAllByText('kitten');
       expect(addedFavoriteWord[0]).toBeInTheDocument();
-  
-      // Check that the button 'Delete' is rendered
+
+      // //delete the word from favorites
       const deleteButton = screen.getByRole('button', { name: 'Delete' });
-      expect(deleteButton).toBeInTheDocument();
-  
-      //delete the word from favorites
-      await act(async () => {
-        fireEvent.click(deleteButton);
-      });
+      await user.click(deleteButton);
+
       // Check that the word has been removed from favorites
       const removedFavoriteWord = screen.queryByRole('h3');
       expect(removedFavoriteWord).not.toBeInTheDocument(); // Only 'Favorites' have an HTML element of h3
